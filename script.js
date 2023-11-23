1b23bb8a262db45447d082c73c0a0d473a4a1264
Emmssz <150143962+Emmssz@users.noreply.github.com>
2023-11-23 13:01:38 +0100

    lagt till function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const check = document.getElementById('divStyle')
 const text = document.querySelectorAll('.textfield')
 const btn = document.getElementsByTagName('button')
 const div = document.getElementByClassName('ettNamn')
+
+function functiondeclare () {
+    
+}

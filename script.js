ab5f563b3495346aae09790091c4c2e4c4f3ea1c
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-11-27 15:33:30 +0100

    js

--- script.js
+++ script.js
@@ -8,13 +8,32 @@ console.log(check);
 console.log(btn);
 console.log(div);
 
-for (let i = 0; i < check.length; i++) {
-    check[i].addEventListener('click', handleCheck);
-}
-
+/*
 function changeColor() {
-    var color = document.getElementById('.input-settings.color').value;
-    var colorBox = document.getElementById('.input-settings.content');
+    var color = document.getElementById('colorInput').value;
+    var colorBox = document.getElementById('colorBox');
     
     colorBox.style.backgroundColor = color;
   }
+*/
+
+document.getElementById("divStyle").addEventListener("click", changeColor);
+
+function changeColor() {
+    var getColor = document.getElementById('color-label').value;
+    var getText = document.getElementById('text-label').value;
+    var box = document.getElementsById('ettNamn')
+
+    box.style.backgroundColor = getColor;
+    box.innerHTML = getText;
+  }
+/*
+  document.getElementById("test").addEventListener("click", changeColor);
+    function changeColor() {
+    var color = document.getElementById('colorInput').value;
+    var text = document.getElementById('textInput').value;
+    var colorBox = document.getElementById('colorBox');
+    
+    colorBox.style.backgroundColor = color;
+    colorBox.innerHTML = text;
+*/

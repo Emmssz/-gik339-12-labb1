4fc7156942235610b1fec2bc0c867ea63048ed3b
Emmssz <150143962+Emmssz@users.noreply.github.com>
2023-11-27 12:32:15 +0100

    commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log(check);
 console.log(btn);
 console.log(div);
 
-for (let i = 0; i < buttons.length; i++) {
+for (let i = 0; i < check.length; i++) {
     buttons[i].addEventListener('click', handleClick);
 
 

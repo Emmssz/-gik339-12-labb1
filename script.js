b0ea46fa0c86f6ec0281242efdfbcf53196f6c72
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-11-27 11:57:30 +0100

    console log

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const check = document.getElementById('divStyle')
 const text = document.querySelectorAll('.textfield')
+const check = document.getElementById('divStyle')
 const btn = document.getElementsByTagName('button')
 const div = document.getElementByClassName('ettNamn')
+
+console.log(text);
+console.log(check);
+console.log(btn);
 
 function functiondeclare () {
     console.log("Hello")

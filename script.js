578b125702e49c17e4f46bb0193a88f0946d29fb
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-11-27 12:37:06 +0100

    css

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const text = document.querySelectorAll('.textfield')
 const check = document.getElementById('divStyle')
 const btn = document.getElementsByTagName('button')
-const div = document.getElementByClassName('ettNamn')
+const div = document.getElementsByClassName('.ettNamn')
 
 console.log(text);
 console.log(check);
 console.log(btn);
-
-
-
-
-

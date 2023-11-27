f0786c9e3547e200b6b6d09ac2255fb36c2abbe1
Emmssz <150143962+Emmssz@users.noreply.github.com>
2023-11-27 16:31:26 +0100

    commit

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const text = document.querySelectorAll('.textfield')
 const check = document.getElementById('divStyle')
 const btn = document.getElementsByTagName('button')
-const div = document.getElementsByClassName('ettNamn')
+const div = document.getElementsByClassName('.ettNamn')
 
 console.log(text);
 console.log(check);
 console.log(btn);
 console.log(div);
 
-/*
-function changeColor() {
+for (let i = 0; i < check.length; i++) {
+    buttons[i].addEventListener('click', handleClick);
+
+}
+
+    function changeColor() {
+        var getColor = document.getElementById('color-label').value;
+        var getText = document.getElementById('text-label').value;
+        var box = document.getElementsById('ettNamn')
+
+        box.style.backgroundColor = getColor;
+        box.innerHTML = getText;
+    }
+
+    /*
+  document.getElementById("test").addEventListener("click", changeColor);
+    function changeColor() {
     var color = document.getElementById('colorInput').value;
+    var text = document.getElementById('textInput').value;
     var colorBox = document.getElementById('colorBox');
     
     colorBox.style.backgroundColor = color;
-  }
+    colorBox.innerHTML = text;
 */
-
-
-
-/* document.getElementById("divStyle").addEventListener("click", changeColor); */
-
-    function changeColor() {
-        var backgroundColor= document.getElementById('backgroundcolor').value;
-        var contentbox = document.getElementById('contentbox');
-        
-        backgroundColor.style.backgroundColor = color;
-
-    }
 
 
 

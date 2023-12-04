3b828020c0585402081e8c647cf8087e35dcbfe1
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-12-04 09:52:39 +0100

    flexbox i css, förslag på js kod

--- script.js
+++ script.js
@@ -36,6 +36,50 @@ function applyChanges() {
 } 
 
 
+/* Nytt förslag (för att få till det med rätt antal variabler, dvs 4, osv)
+// deklarerar variabler, och hämtar in de olika elementen från html-koden
+const checkbox = document.getElementById('divStyle');
+const textFields = document.getElementsByClassName('textfield');
+const removeBtn = document.getElementById('removeBtn');
+const changingDiv = document.getElementById('changingDiv');
+
+// skriver ut avsändaren (target) till konsolen
+function handleInputEvent(e) {
+    console.log('Den här händelsen genererades av:', e.target);
+    const fieldName = e.target.name;
+
+    if (fieldName === 'content') {
+        changingDiv.innerHTML = e.target.value;
+    }
+}
+
+// for-loop som används för att koppla eventlyssnare till text-fields
+for (let i = 0; i < textFields.length; i++) {
+    textFields[i].addEventListener('input', handleInputEvent);
+    textFields[i].addEventListener('blur', handleInputEvent);
+}
+
+// eventlyssnare för när ändringar sker i checkboxen (som en anonym funktion)
+checkbox.addEventListener('change', function () {
+    const colorInput = document.getElementById('color');
+    const color = colorInput.value;
+    changingDiv.style.backgroundColor = color;
+});
+
+// eventlyssnare för när användaren klickar på knappen (som en anonym funktion)
+removeBtn.addEventListener('click', function () {
+    changingDiv.remove();
+});
+*/
+
+
+
+
+
+
+
+
+
 /*const colorInput = document.getElementById('color-label');
 const textInput = document.getElementById('text-label');
 const divBox = document.getElementById('changingDiv');

618a29f5d5d346eb3aea7f604dc1a56a149060cf
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-12-08 13:46:34 +0100

    comments, js

--- script.js
+++ script.js
@@ -23,18 +23,24 @@ function handleInput(e) {
 //UPPGIFT 6
 //for-loop som används för att koppla eventlyssnare till text-fields
 for (let i = 0; i < textFields.length; i++) {
+    //eventlyssnare som reagerar på att användaren skriver in info (vilket skriver ut info i konsolen)
     textFields[i].addEventListener('input', handleInput);
+    //eventlyssnare som reagerar på att användaren lämnar textfältet (vilket skriver ut info i konsolen)
     textFields[i].addEventListener('blur', handleInput);
 }
 
 //eventlyssnare för när ändringar sker i checkboxen (som en anonym funktion)
 checkbox.addEventListener('change', function () {
+    //hämtar in color från html och sparar ner det i en variabel
     const colorInput = document.getElementById('color');
+    //hämtar värdet, det användaren skrev in i färg-textfältet, och sparar i en variabel
     const color = colorInput.value;
+    //ändrar bakgrundsfärgen på div:en enligt det användaren angav (använder variabeln ovan)
     changingDiv.style.backgroundColor = color;
 });
 
 //eventlyssnare för när användaren klickar på knappen (som en anonym funktion)
 removeBtn.addEventListener('click', function () {
+    //använder remove() för att ta bort div:en från sidan
     changingDiv.remove();
 });
